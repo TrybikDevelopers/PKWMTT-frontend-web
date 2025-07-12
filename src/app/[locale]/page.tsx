73697fe64346434044b286af7288fd60d004f5fb
DominikKoniarz <dominik.koniarz.development@gmail.com>
2025-07-12 17:38:44 +0200

    Add colors

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,18 +1,3 @@
-import { Link } from "@/i18n/navigation";
-import { useLocale } from "next-intl";
-
 export default function Home() {
-    const locale = useLocale();
-
-    return (
-        <main className="xs:text-red-500 flex h-full flex-col items-center justify-center gap-4 bg-white text-black md:text-blue-500">
-            <div>Locale: {locale}</div>
-            <Link href="/" locale="en">
-                Go to English
-            </Link>
-            <Link href="/" locale="pl">
-                Go to Polish
-            </Link>
-        </main>
-    );
+    return <main className=""></main>;
 }
