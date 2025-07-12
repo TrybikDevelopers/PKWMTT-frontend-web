e80a05e5f3bf251c098948117fcc26b061830e31
DominikKoniarz <dominik.koniarz.development@gmail.com>
2025-07-12 17:51:59 +0200

    Prototype mobile header

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,3 +1,50 @@
 export default function Home() {
-    return <main className=""></main>;
+    return (
+        <main className="flex h-full w-full flex-col">
+            <div className="text-foreground mb-20 w-full flex-1 overflow-auto">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Exercitationem odio expedita doloribus neque dicta laudantium
+                nulla, necessitatibus consequuntur! Minus iste aliquid cum
+                exercitationem totam quo fugit tenetur incidunt explicabo eum?
+                Quaerat aspernatur tempore laudantium vel doloremque praesentium
+                eligendi officiis earum qui harum enim et quam inventore veniam,
+                maiores est rerum numquam laborum eius. Fugit itaque qui, sit
+                labore rerum sequi. Quo nostrum repellendus alias. Doloribus
+                error, labore ex esse omnis fugiat alias maiores distinctio
+                doloremque pariatur quaerat quibusdam adipisci nesciunt
+                necessitatibus. Asperiores, delectus natus iure itaque harum
+                dolorem doloremque placeat! Ducimus qui ex modi cum quisquam
+                nostrum eveniet excepturi sapiente autem esse eligendi magnam
+                nesciunt officia, minus animi? Rem sunt nam, quisquam nesciunt
+                perferendis sed numquam eum maiores excepturi reiciendis? Animi
+                explicabo placeat, iure, aperiam molestias temporibus eius omnis
+                eum aliquid mollitia ut aspernatur quaerat at perspiciatis
+                incidunt exercitationem delectus accusamus error culpa suscipit,
+                architecto sed eaque facilis. Neque, tempore! Praesentium fugiat
+                porro ducimus nemo iusto quia veritatis officiis exercitationem
+                impedit ut maiores quod, unde quidem repudiandae dicta
+                dignissimos, blanditiis cupiditate velit? Voluptatum,
+                consectetur. Quidem consequuntur delectus amet distinctio
+                consectetur? Corporis illo dicta in perferendis nobis
+                reprehenderit qui, minus quas voluptatibus beatae! Officiis quae
+                cum obcaecati. Dicta, officiis. Consectetur recusandae dolores
+                nobis dolorum doloribus dolore corporis, laboriosam corrupti
+                quas cupiditate! Dolorem, esse repellat! Officiis rem voluptate
+                est nam veritatis! Vitae voluptas deleniti magni delectus
+                obcaecati eius numquam deserunt amet officia, fugit nemo
+                voluptatem omnis possimus inventore voluptatum quas natus
+                provident! Eligendi ut repudiandae sapiente dignissimos aliquam
+                officia quam consequatur possimus nobis ipsam, aspernatur
+                quibusdam, pariatur tenetur tempore sint. Tenetur sint id
+                officia perspiciatis tempora sit sed excepturi nihil consectetur
+                nemo? Perspiciatis reiciendis laborum non consequatur iusto
+                delectus provident nostrum, ea earum aut quod doloribus
+                temporibus consequuntur dolore iste eos dolores eum architecto
+                at magnam neque soluta natus in. Provident, illo?
+            </div>
+            <div className="bg-background-darker fixed right-0 bottom-0 left-0 z-50 h-20 border-t">
+                Mobile header
+            </div>
+        </main>
+    );
 }
