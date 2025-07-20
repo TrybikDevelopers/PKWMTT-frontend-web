3ac9df8d65ffebda4f108ac638f11d7e9d1dc5fb
DominikKoniarz <dominik.koniarz.development@gmail.com>
2025-07-20 13:28:41 +0200

    Close sheet on navigate

--- src/components/header/mobile-nav-sheet/mobile-nav-link.tsx
+++ src/components/header/mobile-nav-sheet/mobile-nav-link.tsx
@@ -1,14 +1,13 @@
-"use client";
-
 import { Link, usePathname } from "@/i18n/navigation";
 import { cn } from "@/lib/utils";
 
 type Props = {
     href: Parameters<typeof Link>[number]["href"];
     children: React.ReactNode;
+    onOpenChange: (open: boolean) => void;
 };
 
-export default function MobileNavLink({ href, children }: Props) {
+export default function MobileNavLink({ href, children, onOpenChange }: Props) {
     const pathname = usePathname();
     const isActive = pathname === href;
 
@@ -22,6 +21,7 @@ export default function MobileNavLink({ href, children }: Props) {
                     ? "bg-button [&>svg]:fill-accent [&>span]:text-foreground"
                     : "bg-button/70 hover:bg-button [&>svg]:fill-accent/70 [&>span]:text-foreground/70 hover:[&>svg]:fill-accent hover:[&>span]:text-foreground",
             )}
+            onNavigate={() => setTimeout(() => onOpenChange(false), 150)}
         >
             {children}
         </Link>
--- src/components/header/mobile-nav-sheet/mobile-nav-sheet.tsx
+++ src/components/header/mobile-nav-sheet/mobile-nav-sheet.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import {
     Sheet,
     SheetContent,
@@ -8,15 +10,25 @@ import {
 } from "@/components/ui/sheet";
 import useNavigationItems from "@/hooks/use-navigation-items";
 import { useTranslations } from "next-intl";
+import { useCallback, useState } from "react";
 import MobileNavLink from "./mobile-nav-link";
 
 export default function MobileNavSheet() {
+    const [open, setOpen] = useState<boolean>(false);
+
     const t = useTranslations("navigation");
 
     const { navigationItems } = useNavigationItems();
 
+    const onOpenChange = useCallback(
+        (open: boolean) => {
+            setOpen(open);
+        },
+        [setOpen],
+    );
+
     return (
-        <Sheet>
+        <Sheet open={open} onOpenChange={onOpenChange}>
             <SheetTrigger className="ml-auto flex h-fit w-fit cursor-pointer flex-col space-y-1 p-2">
                 <div className="h-0.5 w-6 bg-current"></div>
                 <div className="h-0.5 w-6 bg-current"></div>
@@ -33,7 +45,11 @@ export default function MobileNavSheet() {
                 </SheetHeader>
                 <div className="flex flex-col gap-3 px-2">
                     {navigationItems.map(({ href, label, icon }) => (
-                        <MobileNavLink key={href} href={href}>
+                        <MobileNavLink
+                            key={href}
+                            href={href}
+                            onOpenChange={onOpenChange}
+                        >
                             {icon}
                             <span>{label}</span>
                         </MobileNavLink>
