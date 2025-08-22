1ae3a58cd7b854b0e58f9c2da4e4074e529cb538
DominikKoniarz <dominik.koniarz.development@gmail.com>
2025-08-22 23:14:43 +0200

    Add pages metadata

--- src/app/[locale]/calendar/page.tsx
+++ src/app/[locale]/calendar/page.tsx
@@ -1,3 +1,15 @@
+import type { Metadata } from "next";
+import { getTranslations } from "next-intl/server";
+
+export async function generateMetadata(): Promise<Metadata> {
+    const t = await getTranslations("calendar.metadata");
+
+    return {
+        title: t("title"),
+        description: t("description"),
+    };
+}
+
 export default function CalendarPage() {
     return <div>CalendarPage</div>;
 }
