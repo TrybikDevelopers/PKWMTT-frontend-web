c22fe975ea4629106ce8cfc417266fe2742bbe06
DominikKoniarz <dominik.koniarz.development@gmail.com>
2025-10-14 15:51:39 +0200

    Add light mode image

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -8,24 +8,24 @@ export default function Header() {
         <header className="bg-background-darker flex h-18 w-full shrink-0 items-center">
             <Link
                 href="/"
-                className="ml-2 grid aspect-square h-14 w-auto shrink-0 rounded-lg bg-[#1e1e1e] p-1.5 dark:bg-transparent"
+                className="grid aspect-square h-full w-auto shrink-0 rounded-lg p-2.5"
             >
                 <Image
-                    src={"/logo-new.png"}
+                    src={"/logo.png"}
                     width={1024}
                     height={1024}
                     alt="logo"
-                    className="block object-cover"
+                    className="block object-cover dark:hidden"
                     priority
                 />
-                {/* <Image
-                    src={"/logo.png"}
+                <Image
+                    src={"/logo-dark.png"}
                     width={1024}
                     height={1024}
                     alt="logo"
                     className="hidden object-cover dark:block"
                     priority
-                /> */}
+                />
             </Link>
             <div className="flex h-full w-full items-center justify-end p-5">
                 <DesktopNav />
